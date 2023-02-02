e2c474f0f994c985af1187c4926babb0efc50e36
qazwaz <edwardtuzimek@gmail.com>
2023-02-02 15:36:16 +0100

    solved

--- app.js
+++ app.js
@@ -1,41 +1,58 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const users = [{ name: 'Jan Kowalski', job: 'Software Enginner' }];
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/users', (req, res) => {
+  res.json(users)
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get('/users/:id', (req, res, next) => {
+  const id = parseInt(req.params.id);
+  if (users[id]) {
+    res.send(users[id])
+  } else {
+    next(new Error(`No such user with id: ${id}!`))
+  }
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.post('/users', (req, res) => {
+  const user = req.body;
+  users.push(user)
+  res.send(users)
+})
+
+app.put('/users/:id', (req, res, next) => {
+  const id = parseInt(req.params.id);
+  if (users[id]) {
+    const newUser = { ...users[id], ...req.body }
+    users[id] = newUser
+    res.send(users)
+  } else {
+    next(new Error(`No such user with id: ${id}`))
+  }
+})
+
+app.delete('/users/:id', (req, res, next) => {
+  const id = parseInt(req.params.id);
+  if (users[id]) {
+    users.splice(id, 1);
+    res.send(users)
+  } else {
+    next(new Error(`No such user with id: ${id}`))
+  }
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message);
 });
 
-module.exports = app;
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
